307fc700615d35a3e09e13436778b07294d10f03
Alexandre Forel <alex.forel@gmail.com>
2022-06-07 16:08:25 -0400

    Added one test to test_BuildCounterFactualSeekedSet

--- test/test_BuildCounterFactualSeekedSet.py
+++ test/test_BuildCounterFactualSeekedSet.py
@@ -1,5 +1,6 @@
 import unittest
 from pathlib import Path
+import pandas as pd
 # Import local functions to test
 from src.BuildCounterFactualSeekedSet import buildCounterFactualSeekedFile
 
@@ -9,10 +10,14 @@ class test_BuildCounterFactualSeekedSet(unittest.TestCase):
     dataset = 'datasets/Students-Performance-MAT.csv'
     desiredOutcome = 1
     nbCounterFactuals = 5
-    # Manage path to dataset files
+    # Manage path to dataset and counterfactuals files
     THIS_DIR = Path(__file__).parent
     datasetFile = THIS_DIR.parent / dataset
-    print(datasetFile)
+    pathToCounterfactual = datasetFile.parent / 'counterfactuals'
+    datasetName = datasetFile.name
+    counterfactualDatasetFile = pathToCounterfactual / datasetName
+    oneHotDataset = "OneHot_" + datasetName
+    counterfactualOneHotDatasetFile = pathToCounterfactual / oneHotDataset
 
     def test_buildCounterFactualSeekedFile(self):
         """
@@ -24,13 +29,22 @@ class test_BuildCounterFactualSeekedSet(unittest.TestCase):
 
     def test_folderAndFilesCreated(self):
         # Test if the 'counterfactuals' folder has been created
-        pathToCounterfactual = self.datasetFile.parent / 'counterfactuals'
-        self.assertTrue(pathToCounterfactual.exists())
+        self.assertTrue(self.pathToCounterfactual.exists())
         # Test that the counterfactual dataset has been created
-        datasetName = self.datasetFile.name
-        counterfactualDatasetFile = pathToCounterfactual / datasetName
-        self.assertTrue(counterfactualDatasetFile.exists())
+        self.assertTrue(self.counterfactualDatasetFile.exists())
         # Test that the One-Hot counterfactual dataset has been created
-        oneHotDataset = "OneHot_" + datasetName
-        counterfactualOneHotDatasetFile = pathToCounterfactual / oneHotDataset
-        self.assertTrue(counterfactualOneHotDatasetFile.exists())
+        self.assertTrue(self.counterfactualOneHotDatasetFile.exists())
+
+    def test_counterfactualDataset(self):
+        """ Test size of the counterfactual dataset built."""
+        # Dataset
+        counterfactualsData = pd.read_csv(self.counterfactualDatasetFile)
+        nbFeaturesInStudentDataSet = 30
+        self.assertTrue(counterfactualsData.size
+                        == self.nbCounterFactuals * (nbFeaturesInStudentDataSet+1))
+        # One-hot encoded dataset
+        oneHotCounterfactualsData = pd.read_csv(
+            self.counterfactualOneHotDatasetFile)
+        nbFeaturesInOneHotStudentDataSet = 43
+        self.assertTrue(oneHotCounterfactualsData.size
+                        == self.nbCounterFactuals * (nbFeaturesInOneHotStudentDataSet+1))
