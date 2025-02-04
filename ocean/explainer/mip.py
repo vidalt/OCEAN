612d8cda38ab759e7e0e89d34d76d5d3dd6aac6d
eminyous <youssouf.emine@polymtl.ca>
2025-02-04 15:21:52 -0500

    Increase n_estimators to 100 and add cleanup method to MIPExplainer for better resource management

--- ocean/explainer/mip.py
+++ ocean/explainer/mip.py
@@ -11,6 +11,8 @@ class MIPExplainer(Model):
     _ensemble: Ensemble
     _mapper: FeatureMapper
 
+    _garbage: list[gp.Var | gp.MVar | gp.Constr | gp.MConstr]
+
     def __init__(
         self,
         ensemble: BaseEnsemble,
@@ -39,16 +41,21 @@ class MIPExplainer(Model):
             flow_type=flow_type,
         )
         Model.build(self)
+        self._garbage = []
 
     def add_objective(
         self,
         x: FloatArray1D,
         *,
-        norm: int = 2,
+        norm: int = 1,
         sense: int = gp.GRB.MINIMIZE,
     ) -> None:
         objective = self._add_objective(x=x, norm=norm)
         self.setObjective(objective, sense=sense)
+
+    def cleanup(self) -> None:
+        self.remove(self._garbage)
+        self._garbage.clear()
 
     def _add_objective(
         self,
@@ -57,6 +64,8 @@ class MIPExplainer(Model):
     ) -> gp.LinExpr | gp.QuadExpr:
         series = pd.Series(x, index=self._mapper.columns)
         match norm:
+            case 1:
+                return self._add_l1(series)
             case 2:
                 return self._add_l2(series)
             case _:
@@ -75,3 +84,29 @@ class MIPExplainer(Model):
                     value = series.xs((name, code), level=(0, 1)).to_numpy()[0]
                     obj += (var[code] - value) ** 2
         return obj
+
+    def _add_l1(self, series: "pd.Series[float]") -> gp.LinExpr:
+        value: float = 0.0
+        n_vars = len(series)
+        u = self.addMVar(n_vars, name="u")
+        self._garbage.append(u)
+        for name, var in self.features.items():
+            if not var.is_one_hot_encoded:
+                value = series.xs(name, level=0).to_numpy()[0]
+                if series.index.nlevels == 1:
+                    idx = series.index.to_list().index(name)
+                else:
+                    idx = series.index.to_list().index((name, ""))
+                cons = self.addConstr(var.x - value <= u[idx])
+                self._garbage.append(cons)
+                cons = self.addConstr(var.x - value >= -u[idx])
+                self._garbage.append(cons)
+            else:
+                for code in var.codes:
+                    value = series.xs((name, code), level=(0, 1)).to_numpy()[0]
+                    idx = series.index.to_list().index((name, code))
+                    cons = self.addConstr(var[code] - value <= u[idx])
+                    self._garbage.append(cons)
+                    cons = self.addConstr(var[code] - value >= -u[idx])
+                    self._garbage.append(cons)
+        return u.sum().item()
