2c640b59c24da49e3d686779ecf6ad2a19128c7d
eminyous <youssouf.emine@polymtl.ca>
2025-02-04 14:16:45 -0500

    Add Random Forest example and update .gitignore to exclude example data

---
+++ examples/random_forest.py
@@ -0,0 +1,174 @@
+import time
+from pathlib import Path
+
+import gurobipy as gp
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import (
+    train_test_split,  # pyright: ignore[reportUnknownVariableType]
+)
+
+from ocean import MIPExplainer
+from ocean.feature import Feature, FeatureMapper
+from ocean.typing import FloatArray1D
+
+file = Path(__file__).parent / "data" / "default_credit_numerical.csv"
+target = "DEFAULT_PAYEMENT"
+
+print(f"Reading data from {file}")  # noqa: T201
+data = pd.read_csv(file)
+X, y = (  # pyright: ignore[reportUnknownVariableType]
+    data.drop(columns=target).to_numpy(),
+    data[target].astype(int).to_numpy(),
+)
+print("Data loaded")  # noqa: T201
+
+features: list[Feature] = []
+names: list[str] = []
+columns: "pd.Index[str] | pd.MultiIndex" = pd.Index([""])
+
+
+names.extend([
+    "LIMIT_BAL",
+    "AGE",
+    "BILL_AMT1",
+    "BILL_AMT2",
+    "BILL_AMT3",
+    "BILL_AMT4",
+    "BILL_AMT5",
+    "BILL_AMT6",
+    "PAY_AMT1",
+    "PAY_AMT2",
+    "PAY_AMT3",
+    "PAY_AMT4",
+    "PAY_AMT5",
+    "PAY_AMT6",
+    "Sex",
+    "Education",
+    "Marriage",
+    "Pay_0",
+    "Pay_2",
+    "Pay_3",
+    "Pay_4",
+    "Pay_5",
+    "Pay_6",
+])
+features.extend([
+    Feature(ftype=Feature.Type.CONTINUOUS, levels=(10000 - 1, 1000000 + 1)),
+    Feature(ftype=Feature.Type.DISCRETE, levels=range(21, 80)),
+    Feature(ftype=Feature.Type.CONTINUOUS, levels=(-165580 - 1, 964511 + 1)),
+    Feature(ftype=Feature.Type.CONTINUOUS, levels=(-69777 - 1, 983931 + 1)),
+    Feature(ftype=Feature.Type.CONTINUOUS, levels=(-157264 - 1, 1664089 + 1)),
+    Feature(ftype=Feature.Type.CONTINUOUS, levels=(-170000 - 1, 891586 + 1)),
+    Feature(ftype=Feature.Type.CONTINUOUS, levels=(-81334 - 1, 927171 + 1)),
+    Feature(ftype=Feature.Type.CONTINUOUS, levels=(-339603 - 1, 961664 + 1)),
+    Feature(ftype=Feature.Type.CONTINUOUS, levels=(0 - 1, 873552 + 1)),
+    Feature(ftype=Feature.Type.CONTINUOUS, levels=(0 - 1, 1684259 + 1)),
+    Feature(ftype=Feature.Type.CONTINUOUS, levels=(0 - 1, 896040 + 1)),
+    Feature(ftype=Feature.Type.CONTINUOUS, levels=(0 - 1, 621000 + 1)),
+    Feature(ftype=Feature.Type.CONTINUOUS, levels=(0 - 1, 426529 + 1)),
+    Feature(ftype=Feature.Type.CONTINUOUS, levels=(0 - 1, 528666 + 1)),
+    Feature(ftype=Feature.Type.BINARY),
+    Feature(
+        ftype=Feature.Type.ONE_HOT_ENCODED,
+        codes=("Graduate_School", "High_School", "Others", "University"),
+    ),
+    Feature(
+        ftype=Feature.Type.ONE_HOT_ENCODED,
+        codes=("Married", "Others", "Single"),
+    ),
+    Feature(ftype=Feature.Type.BINARY),
+    Feature(ftype=Feature.Type.BINARY),
+    Feature(ftype=Feature.Type.BINARY),
+    Feature(ftype=Feature.Type.BINARY),
+    Feature(ftype=Feature.Type.BINARY),
+    Feature(ftype=Feature.Type.BINARY),
+])
+
+columns = pd.MultiIndex.from_tuples([
+    ("LIMIT_BAL", ""),
+    ("AGE", ""),
+    ("BILL_AMT1", ""),
+    ("BILL_AMT2", ""),
+    ("BILL_AMT3", ""),
+    ("BILL_AMT4", ""),
+    ("BILL_AMT5", ""),
+    ("BILL_AMT6", ""),
+    ("PAY_AMT1", ""),
+    ("PAY_AMT2", ""),
+    ("PAY_AMT3", ""),
+    ("PAY_AMT4", ""),
+    ("PAY_AMT5", ""),
+    ("PAY_AMT6", ""),
+    ("Sex", ""),
+    ("Education", "Graduate_School"),
+    ("Education", "High_School"),
+    ("Education", "Others"),
+    ("Education", "University"),
+    ("Marriage", "Married"),
+    ("Marriage", "Others"),
+    ("Marriage", "Single"),
+    ("Pay_0", ""),
+    ("Pay_2", ""),
+    ("Pay_3", ""),
+    ("Pay_4", ""),
+    ("Pay_5", ""),
+    ("Pay_6", ""),
+])
+
+
+mapper = FeatureMapper(names=names, features=features, columns=columns)
+
+X_train, X_test, y_train, y_test = train_test_split(  # pyright: ignore[reportUnknownVariableType]
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+)
+
+# Fit the Random Forest model
+print("Fitting a Random Forest model")  # noqa: T201
+rf = RandomForestClassifier(n_estimators=5, random_state=42, max_depth=3)
+rf.fit(X_train, y_train)  # pyright: ignore[reportUnknownArgumentType]
+print("Model fitted")  # noqa: T201
+
+# Evaluate the model
+print("Building the MIPExplainer")  # noqa: T201
+env = gp.Env(empty=True)
+env.setParam("OutputFlag", 0)
+env.start()
+start = time.time()
+mip = MIPExplainer(rf, mapper=mapper, env=env)
+end = time.time()
+print("MIPExplainer built")  # noqa: T201
+
+elapsed = end - start
+
+# Generate multiple queries:
+queries: list[tuple[FloatArray1D, int]] = []
+
+y_pred = rf.predict(X_test)  # pyright: ignore[reportUnknownVariableType, reportUnknownArgumentType]
+
+print("Generating queries")  # noqa: T201
+for x, y_ in zip(X_test, y_pred, strict=True):  # pyright: ignore[reportUnknownVariableType, reportUnknownArgumentType]
+    queries.append((x, 1 - y_))  # pyright: ignore[reportUnknownArgumentType]
+print("Queries generated")  # noqa: T201
+
+print("Running queries")  # noqa: T201
+times: list[float] = []
+for x, class_ in queries:
+    start = time.time()
+    mip.add_objective(x)
+    mip.set_majority_class(class_)
+    mip.optimize()
+    mip.clear_majority_class()
+    end = time.time()
+
+    times.append(end - start)
+print("Queries run")  # noqa: T201
+
+print(f"Buiding the model took {elapsed:.2f} seconds")  # noqa: T201
+print(f"Average time per query: {sum(times) / len(times):.2f} seconds")  # noqa: T201
+print(f"Maximum time per query: {max(times):.2f} seconds")  # noqa: T201
+print(f"Minimum time per query: {min(times):.2f} seconds")  # noqa: T201
+print(f"Total number of queries: {len(queries)}")  # noqa: T201
