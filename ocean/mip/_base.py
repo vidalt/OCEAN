695e282ba8e75c1af8de22501dee0b87110cdb88
eminyous <youssouf.emine@polymtl.ca>
2025-03-06 14:04:19 -0500

    Update __setattr__ method to accept Any type for value

--- ocean/mip/_base.py
+++ ocean/mip/_base.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from typing import Protocol
+from typing import Any, Protocol
 
 import gurobipy as gp
 
@@ -8,7 +8,7 @@ class BaseModel(ABC, gp.Model):
     def __init__(self, name: str = "", env: gp.Env | None = None) -> None:
         gp.Model.__init__(self, name=name, env=env)
 
-    def __setattr__(self, name: str, value: int) -> None:
+    def __setattr__(self, name: str, value: Any) -> None:  # noqa: ANN401
         object.__setattr__(self, name, value)
 
     def build_vars(self, *variables: "Var") -> None:
