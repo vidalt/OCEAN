f261710dd945cc84d3436a24bba1ade2f76c72ac
eminyous <youssouf.emine@polymtl.ca>
2025-03-06 14:54:01 -0500

    Add FeatureVar and TreeVar classes for constraint programming

---
+++ ocean/cp/_variables/_feature.py
@@ -0,0 +1,69 @@
+from ortools.sat.python import cp_model as cp
+
+from ...feature import Feature
+from ...feature._keeper import FeatureKeeper
+from ...typing import Key
+from .._base import BaseModel, Var
+
+
+class FeatureVar(Var, FeatureKeeper):
+    X_VAR_NAME_FMT: str = "x[{name}]"
+
+    _x: cp.IntVar
+    _u: dict[Key, cp.IntVar]
+
+    def __init__(self, feature: Feature, name: str) -> None:
+        Var.__init__(self, name=name)
+        FeatureKeeper.__init__(self, feature=feature)
+
+    def build(self, model: BaseModel) -> None:
+        if self.is_one_hot_encoded:
+            u = self._add_u(model)
+            model.Add(cp.LinearExpr.Sum(u.values()) == 1)
+            self._u = u
+            return
+        self._x = self._add_x(model)
+
+    def xget(self, code: Key | None = None) -> cp.IntVar:
+        if self.is_one_hot_encoded:
+            if code is None:
+                msg = "Code was not provided for one-hot encoded feature"
+                raise ValueError(msg)
+            return self._u[code]
+
+        if code is not None:
+            msg = "Get by code is only supported for one-hot encoded features"
+            raise ValueError(msg)
+
+        return self._x
+
+    def _add_x(self, model: BaseModel) -> cp.IntVar:
+        name = self.X_VAR_NAME_FMT.format(name=self._name)
+
+        # Case when the feature is binary.
+        if self.is_binary:
+            return self._add_binary(model, name)
+
+        # Case when the feature is continuous or discrete.
+        return self._add_numeric(model, name)
+
+    def _add_u(self, model: BaseModel) -> dict[Key, cp.IntVar]:
+        name = self.X_VAR_NAME_FMT.format(name=self._name)
+        return self._add_one_hot_encoded(model=model, name=name)
+
+    def _add_one_hot_encoded(
+        self,
+        model: BaseModel,
+        name: str,
+    ) -> dict[Key, cp.IntVar]:
+        return {
+            code: model.NewBoolVar(f"{name}[{code}]") for code in self.codes
+        }
+
+    @staticmethod
+    def _add_binary(model: BaseModel, name: str) -> cp.IntVar:
+        return model.NewBoolVar(name)
+
+    def _add_numeric(self, model: BaseModel, name: str) -> cp.IntVar:
+        m = len(self.levels)
+        return model.NewIntVar(0, m - 1, name)
