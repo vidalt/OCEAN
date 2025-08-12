25b41f36b8766ea473b93105806b0ef0f9613658
Awa Khouna <awa.khouna@polytechnique.edu>
2025-08-12 17:28:09 -0400

    Added query management to the Explainer class and updated the Explanation class to include the query property.

--- ocean/cp/_explanation.py
+++ ocean/cp/_explanation.py
@@ -11,7 +11,8 @@ from ._variables import FeatureVar
 
 
 class Explanation(Mapper[FeatureVar], BaseExplanation):
-    _epsilon: float = 1e-8
+    _epsilon: float = 1e-3
+    _x: Array1D = np.zeros((0,), dtype=int)
 
     def vget(self, i: int) -> cp.IntVar:
         name = self.names[i]
@@ -28,11 +29,12 @@ class Explanation(Mapper[FeatureVar], BaseExplanation):
             name = self.names[f]
             value = ENV.solver.Value(self.vget(f))
             if self[name].is_continuous:
-                value = self[name].levels[value + 1]
-                values[f] = value - self._epsilon
+                values[f] = self.format_value(
+                    f, int(value), list(self[name].levels)
+                )
             elif self[name].is_discrete:
                 value = self[name].levels[value]
-                values[f] = value - self._epsilon
+                values[f] = value
         return pd.Series(values, index=self.columns)
 
     def to_numpy(self) -> Array1D:
@@ -62,12 +64,35 @@ class Explanation(Mapper[FeatureVar], BaseExplanation):
                 idx = int(solver.Value(v.xget()))
                 return float(v.levels[idx])
             if v.is_continuous:
+                f = [val for _, val in self.items()].index(v)
                 idx = int(solver.Value(v.xget()))
-                return float(v.levels[idx + 1])
+                return self.format_value(f, idx, list(v.levels))
             x = v.xget()
             return solver.Value(x)
 
         return self.reduce(get)
+
+    def format_value(self, f: int, idx: int, levels: list[float]) -> float:
+        if self.query.shape[0] == 0:
+            return float(levels[idx] + levels[idx + 1]) / 2
+        j = 0
+        while self.query[f] > levels[j + 1]:
+            j += 1
+        if j == idx:
+            value = float(self.query[f])
+        elif j < idx:
+            value = float(levels[idx]) + self._epsilon
+        else:
+            value = float(levels[idx + 1]) - self._epsilon
+        return value
+
+    @property
+    def query(self) -> Array1D:
+        return self._x
+
+    @query.setter
+    def query(self, value: Array1D) -> None:
+        self._x = value
 
     def __repr__(self) -> str:
         mapping = self.value
