c21d68cefd037ee4f7d3e898c067b333d74b3b49
jferry <julienferry12@gmail.com>
2025-08-19 11:28:15 -0400

    cleans example file

--- examples/simple_example_both.py
+++ examples/simple_example_both.py
@@ -9,25 +9,6 @@ from ocean.datasets import load_adult
 
 warnings.filterwarnings("ignore")
 
-gurobi_statuses = {
-    1: "LOADED",
-    2: "OPTIMAL",
-    3: "INFEASIBLE",
-    4: "INF_OR_UNBD",
-    5: "UNBOUNDED",
-    6: "CUTOFF",
-    7: "ITERATION_LIMIT",
-    8: "NODE_LIMIT",
-    9: "TIME_LIMIT",
-    10: "SOLUTION_LIMIT",
-    11: "INTERRUPTED",
-    12: "NUMERIC",
-    13: "SUBOPTIMAL",
-    14: "INPROGRESS",
-    15: "USER_OBJ_LIMIT",
-    16: "WORK_LIMIT",
-}  # see https://www.gurobi.com/documentation/9.5/refman/optimization_status_codes.html#sec:StatusCodes
-
 plot_anytime_distances = True
 num_workers = 4  # Both CP and MILP solving support multithreading
 random_state = 42
