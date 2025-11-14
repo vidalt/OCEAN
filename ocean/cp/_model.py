Improves continuous feature distance computation using AddElement, improves and fixes ordinal features distance computation

ocean/cp/_model.py
@@ -125,7 +125,7 @@ class Model(BaseModel, FeatureManager, TreeManager, GarbageManager):
         k = 0
         for v in variables:
             if v.is_one_hot_encoded:
-                for code in v.codes:
+                for code in sorted(v.codes, key=int):
                     objective += self.L1(x_arr[k], v, code=code)
                     k += 1
             else:
@@ -157,10 +157,28 @@ class Model(BaseModel, FeatureManager, TreeManager, GarbageManager):
         if v.is_numeric:
             if v.is_continuous:
                 intervals_cost = self.get_intervals_cost(v.levels, x)
-                variables = [v.mget(i) for i in range(len(v.levels) - 1)]
-                obj_expr = cp.LinearExpr.WeightedSum(variables, intervals_cost)
+                # tighten domain of objvar based on x itself ----------
+                v.objvarget().Proto().domain[:] = []
+                v.objvarget().Proto().domain.extend( cp.Domain(min(intervals_cost), max(intervals_cost)).FlattenedIntervals())    
+                # -----------------------------------------------------
+                obj_expr = v.objvarget()
+                self.add_garbage(
+                    self.AddElement(v.xget(), list(intervals_cost), obj_expr)
+                )
                 obj_coefs.append(1)
             else:
+                # include the value of x itself on the domain ---------
+                if len(v.thresholds) != 0:
+                    values = [
+                        val for v in v.thresholds for val in [int(v), int(v) + 1]
+                    ]
+                else:
+                    values = np.asarray(v.levels).astype(int).tolist()
+                values.append(int(x))
+                #print("new values for discrete feature ", v._name, ": ", values) # debug
+                v.xget().Proto().domain[:] = []
+                v.xget().Proto().domain.extend( cp.Domain.FromValues(values).FlattenedIntervals())    
+                # -----------------------------------------------------
                 obj_expr = v.objvarget()
                 self.add_garbage(
                     self.AddAbsEquality(obj_expr, int(x) - v.xget())

ocean/cp/_variables/_feature.py
@@ -14,7 +14,6 @@ class FeatureVar(Var, FeatureKeeper):
 
     _x: cp.IntVar
     _u: Mapping[Key, cp.IntVar]
-    _mu: list[cp.IntVar]
     _objvar: cp.IntVar
 
     def __init__(self, feature: Feature, name: str) -> None:
@@ -24,10 +23,7 @@ class FeatureVar(Var, FeatureKeeper):
     def build(self, model: BaseModel) -> None:
         if not self.is_one_hot_encoded:
             self._x = self._add_x(model)
-        if self.is_continuous:
-            self._mu = self._add_mu(model)
-            model.add_map_domain(self.xget(), self._mu)
-        elif self.is_one_hot_encoded:
+        if self.is_one_hot_encoded:
             self._u = self._add_u(model)
 
     def xget(self, code: Key | None = None) -> cp.IntVar:
@@ -38,16 +34,10 @@ class FeatureVar(Var, FeatureKeeper):
             raise ValueError(msg)
         return self._x
 
-    def mget(self, key: int) -> cp.IntVar:
-        if not self.is_continuous:
-            msg = "The 'mget' method is only supported for continuous features"
-            raise ValueError(msg)
-        return self._mu[key]
-
     def objvarget(self) -> cp.IntVar:
-        if not self.is_discrete:
+        if not self.is_discrete and not self.is_continuous:
             msg = (
-                "The 'objvarget' method is only supported for discrete features"
+                "The 'objvarget' method is only supported for continuous and discrete features"
             )
             raise ValueError(msg)
         return self._objvar
@@ -76,16 +66,6 @@ class FeatureVar(Var, FeatureKeeper):
         model.AddExactlyOne(u.values())
         return u
 
-    def _set_mu(self, model: BaseModel, m: int) -> list[cp.IntVar]:
-        return [model.NewBoolVar(f"{self._name}_mu_{i}") for i in range(m)]
-
-    def _add_mu(self, model: BaseModel) -> list[cp.IntVar]:
-        if not self.is_continuous:
-            msg = "Mu variables are only supported for continuous features"
-            raise ValueError(msg)
-        m = len(self.levels) - 1
-        return self._set_mu(model, m=m)
-
     def _add_one_hot_encoded(
         self,
         model: BaseModel,
@@ -100,6 +80,7 @@ class FeatureVar(Var, FeatureKeeper):
         return model.NewBoolVar(name)
 
     def _add_continuous(self, model: BaseModel, name: str) -> cp.IntVar:
+        self._objvar = model.NewIntVar(0, int(self.levels[-1]), f"u_{name}")
         m = len(self.levels)
         return model.NewIntVar(0, m - 2, name)
 
@@ -110,6 +91,7 @@ class FeatureVar(Var, FeatureKeeper):
             ]
         else:
             values = np.asarray(self.levels).astype(int).tolist()
+        #print("values for discrete feature ", self._name, ": ", values) #debug
         val = cp.Domain.FromValues(values)
         self._objvar = model.NewIntVar(0, int(self.levels[-1]), f"u_{name}")
         return model.NewIntVarFromDomain(val, name)