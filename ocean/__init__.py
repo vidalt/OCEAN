f7b5abe1091ee5e332c1ec4c995cd0f83b5570b3
Awa Khouna <awa.khouna@polytechnique.edu>
2026-06-23 14:31:42 -0400

    feat: Add DLS and SLS explainers to the entry point of the OCEAN package

--- ocean/__init__.py
+++ ocean/__init__.py
@@ -1,15 +1,18 @@
 """Public package entry points for OCEAN."""
 
-from . import abc, cp, datasets, feature, maxsat, mip, tree
+from . import abc, cp, datasets, feature, ls, maxsat, mip, tree
 
 MixedIntegerProgramExplainer = mip.Explainer
 ConstraintProgrammingExplainer = cp.Explainer
 MaxSATExplainer = maxsat.Explainer
-
+DLSExplainer = ls.DLSExplainer
+SLSExplainer = ls.SLSExplainer
 __all__ = [
     "ConstraintProgrammingExplainer",
+    "DLSExplainer",
     "MaxSATExplainer",
     "MixedIntegerProgramExplainer",
+    "SLSExplainer",
     "abc",
     "cp",
     "datasets",
